54ef7a54a0a4a5f0444c95a5524747405e4f4f68
VakhitovVladislav <67861615+VakhitovVladislav@users.noreply.github.com>
2023-09-17 13:23:26 +0800

    code structuring completed

---
+++ sandbox/sandbox/src/main/java/ru/stqa/geometry/Geometry.java
@@ -0,0 +1,14 @@
+package ru.stqa.geometry;
+import ru.stqa.geometry.figures.Rectange;
+import ru.stqa.geometry.figures.Square;
+
+
+public class Geometry {
+
+
+    public static void main(String[] args) {
+        
+    }
+
+
+}
