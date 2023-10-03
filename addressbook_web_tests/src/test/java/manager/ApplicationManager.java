e3e32b9e620fa6d7763a447679ddd972654042ef
VakhitovVladislav <67861615+VakhitovVladislav@users.noreply.github.com>
2023-10-03 20:15:17 +0800

    add Firefox browser for tests

---
+++ addressbook_web_tests/src/test/java/manager/ApplicationManager.java
@@ -0,0 +1,57 @@
+package manager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class ApplicationManager {
+
+    private LoginHelper session;
+
+    protected  WebDriver driver;
+
+    private GroupHelper groups;
+
+    public void init(String browser) {
+        if (driver == null) {
+            if("chrome".equals(browser)) {
+                driver = new ChromeDriver();
+            } else if("firefox".equals(browser)) {
+                driver = new FirefoxDriver();
+            } else {
+                throw new IllegalArgumentException(String.format("Unknown browser %s", browser));
+            }
+            Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
+            driver.get("http://localhost/addressbook/");
+            driver.manage().window().setSize(new Dimension(1600, 984));
+            session().login("admin", "secret");
+        }
+    }
+
+    public GroupHelper groups(){
+        if (groups == null){
+            groups = new GroupHelper(this);
+        }
+        return groups;
+    }
+
+    public LoginHelper session(){
+        if (session == null){
+            session = new LoginHelper(this);
+        }
+        return session;
+    }
+
+    protected boolean isElementPresent(By locator) {
+        try {
+            driver.findElement(locator);
+            return true;
+        } catch (NoSuchElementException exception) {
+            return false;
+        }
+    }
+
+}
